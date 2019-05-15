3a2a32a323a4559ba47aae5da9b3433a924093c7
wangxueting <xueting.wang@zebra-c.com>
2019-05-15 09:34:50 +0800

    完善

--- src/views/product/index.vue
+++ src/views/product/index.vue
@@ -10,158 +10,49 @@
           :gutter="24"
           class="row-space"
         >
-          <el-col :span="6">
+          <el-col :span="6" v-for="(item, index) in list" :key="index">
             <div class="grid-content bg-purple">
-              <div class="product-up__product product-up__transform">
+              <div class="product-up__product product-up__transform" @mouseenter="item.show = true" @mouseleave="item.show = false" >
                 <div>
                   <img
                     class="product-up__img"
-                    src="../../assets/images/product/1-1.png"
+                    :src="item.img"
                     alt=""
                   >
                 </div>
-                <div class="product-up__box">
-                  <span class="product-up__tip">
-                    wifi-pen除具备基本智能点读功能外，通过逻辑分析与数字运算技术，对英文，数字，算符进行书写、识别，并且实现语音播放等功能。
+                <div class="product-up__box" name='slide-fade'>
+                  <span class="product-up__tip" v-if="item.show">
+                    {{item.content}}
                   </span>
                 </div>
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content bg-purple">
-              <div class="product-up__product product-up__transform">
-                <div>
-                  <img
-                    class="product-up__img"
-                    src="../../assets/images/product/1-2.png"
-                    alt=""
-                  >
-                </div>
-                <div class="product-up__box">
-                  <span class="product-up__tip">
-                    G-Speak高音质智能点读笔采用干电池供电，在智能点读功能基本上，实现录音与语音播放、自定义游戏及便签或互动问答等功能。
-                  </span>
-                </div>
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content bg-purple">
-              <div class="product-up__product product-up__transform">
-                <div class="product-up__product">
-                  <div>
-                    <img
-                      class="product-up__img"
-                      src="../../assets/images/product/1-3.png"
-                      alt=""
-                    >
-                  </div>
-                  <div class="product-up__box">
-                    <span class="product-up__tip">
-                      G-Talk高音质智能点读笔采用锂电池供电。在智能点读功能基本上，实现录音与语音播放、自定义游戏及便签或互动问答等功能。
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content bg-purple">
-              <div class="product-up__product product-up__transform">
-                <div class="product-up__product">
-                  <div>
-                    <img
-                      class="product-up__img"
-                      src="../../assets/images/product/1-4.png"
-                      alt=""
-                    >
-                  </div>
-                  <div class="product-up__box">
-                    <span class="product-up__tip">
-                      G-PEN智能USB扫描手写笔，通过USB连接，将书写、绘画等内容实时传输智能设别，实现设备与纸质媒体的相结合，并可作为高精度高速光学鼠标使用。因具有点阵码扫描功能，即可对防伪码进行识别。
-                    </span>
-                  </div>
+                <div class="product-up__titles">
+                  {{item.title}}
                 </div>
               </div>
             </div>
           </el-col>
         </el-row>
-        <el-row :gutter="24">
-          <el-col :span="6">
+        <el-row
+          :gutter="24"
+          class="row-space"
+        >
+          <el-col :span="6" v-for="(item, index) in list2" :key="index">
             <div class="grid-content bg-purple">
-              <div class="product-up__product product-up__transform">
+              <div class="product-up__product product-up__transform" @mouseenter="item.show = true" @mouseleave="item.show = false" >
                 <div>
                   <img
                     class="product-up__img"
-                    src="../../assets/images/product/1-5.png"
+                    :src="item.img"
                     alt=""
                   >
                 </div>
-                <div class="product-up__box">
-                  <span class="product-up__tip">
-                    G-PEN智能USB扫描手写笔，通过USB连接，将书写、绘画等内容实时传输智能设别，实现设备与纸质媒体的相结合，并可作为高精度高速光学鼠标使用。因具有点阵码扫描功能，即可对防伪码进行识别。
+                <div class="product-up__box" name='slide-fade'>
+                  <span class="product-up__tip" v-if="item.show">
+                    {{item.content}}
                   </span>
                 </div>
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content bg-purple">
-              <div class="product-up__product product-up__transform">
-                <div class="product-up__product">
-                  <div>
-                    <img
-                      class="product-up__img"
-                      src="../../assets/images/product/1-6.png"
-                      alt=""
-                    >
-                  </div>
-                  <div class="product-up__box">
-                    <span class="product-up__tip">
-                      G-PEN2是G-PEN智能USB手写笔的升级版。通过USB免驱连接电脑的画图板或其他应用软件互联，具备智能手写、绘画等功能并实现实时传输，还可作为高精度高速光学鼠标使用
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content bg-purple">
-              <div class="product-up__product product-up__transform">
-                <div class="product-up__product">
-                  <div>
-                    <img
-                      class="product-up__img"
-                      src="../../assets/images/product/1-7.png"
-                      alt=""
-                    >
-                  </div>
-                  <div class="product-up__box">
-                    <span class="product-up__tip">
-                      BT-PEN作为一款智能手写蓝牙笔，可通过笔迹智能识别，实现智能书写、绘画并可一键生成电子数据。同时可配合纳米窄带光学薄膜，满足高性价比高性能教育会议电视电子白板等媒介的输入需求
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content bg-purple">
-              <div class="product-up__product product-up__transform">
-                <div class="product-up__product">
-                  <div>
-                    <img
-                      class="product-up__img"
-                      src="../../assets/images/product/1-8.png"
-                      alt=""
-                    >
-                  </div>
-                  <div class="product-up__box">
-                    <span class="product-up__tip">
-                      AI多功能笔通过WIFI同步互联可对海量云端资源进行在线智能点读、问答及游戏互动。现已实现与微信实时连接，进行语音对话、资源传输、云端资源共享等功能。
-                    </span>
-                  </div>
+                <div class="product-up__titles">
+                  {{item.title}}
                 </div>
               </div>
             </div>
@@ -305,8 +196,73 @@
 </template>
 <script>
 import homeBanner from 'components/banner'
+import pic1 from '../../assets/images/product/1-1.png'
+import pic2 from '../../assets/images/product/1-2.png'
+import pic3 from '../../assets/images/product/1-3.png'
+import pic4 from '../../assets/images/product/1-4.png'
+import pic5 from '../../assets/images/product/1-5.png'
+import pic6 from '../../assets/images/product/1-6.png'
+import pic7 from '../../assets/images/product/1-7.png'
+import pic8 from '../../assets/images/product/1-8.png'
 export default {
   name: 'news',
+  data () {
+    return {
+      show: false,
+      list: [
+        {
+          title: 'wifi-pen智能点读笔',
+          content: 'wifi-pen除具备基本智能点读功能外，通过逻辑分析与数字运算技术，对英文，数字，算符进行书写、识别，并且实现语音播放等功能。',
+          img: pic1,
+          show: false
+        },
+        {
+          title: 'G-Speak高音质智能点读笔',
+          content: 'G-Speak高音质智能点读笔采用干电池供电，在智能点读功能基本上，实现录音与语音播放、自定义游戏及便签或互动问答等功能。',
+          img: pic2,
+          show: false
+        },
+        {
+          title: 'G-Talk高音质智能点读笔',
+          content: 'G-Talk高音质智能点读笔采用锂电池供电。在智能点读功能基本上，实现录音与语音播放、自定义游戏及便签或互动问答等功能。',
+          img: pic3,
+          show: false
+        },
+        {
+          title: 'G-PEN智能USB扫描手写笔',
+          content: 'G-PEN智能USB扫描手写笔，通过USB连接，将书写、绘画等内容实时传输智能设别，实现设备与纸质媒体的相结合，并可作为高精度高速光学鼠标使用。因具有点阵码扫描功能，即可对防伪码进行识别。',
+          img: pic4,
+          show: false
+        }
+      ],
+      list2: [
+        {
+          title: 'G-PEN智能USB扫描手写笔',
+          content: 'G-PEN智能USB扫描手写笔，通过USB连接，将书写、绘画等内容实时传输智能设别，实现设备与纸质媒体的相结合，并可作为高精度高速光学鼠标使用。因具有点阵码扫描功能，即可对防伪码进行识别。',
+          img: pic5,
+          show: false
+        },
+        {
+          title: 'G-PEN2智能USB手写笔',
+          content: 'G-PEN2是G-PEN智能USB手写笔的升级版。通过USB免驱连接电脑的画图板或其他应用软件互联，具备智能手写、绘画等功能并实现实时传输，还可作为高精度高速光学鼠标使用',
+          img: pic6,
+          show: false
+        },
+        {
+          title: 'BT-PEN智能手写蓝牙笔',
+          content: 'BT-PEN作为一款智能手写蓝牙笔，可通过笔迹智能识别，实现智能书写、绘画并可一键生成电子数据。同时可配合纳米窄带光学薄膜，满足高性价比高性能教育会议电视电子白板等媒介的输入需求',
+          img: pic7,
+          show: false
+        },
+        {
+          title: 'AI多功能笔',
+          content: 'AI多功能笔通过WIFI同步互联可对海量云端资源进行在线智能点读、问答及游戏互动。现已实现与微信实时连接，进行语音对话、资源传输、云端资源共享等功能。',
+          img: pic8,
+          show: false
+        }
+      ]
+    }
+  },
   components: {
     homeBanner
   }
@@ -319,7 +275,7 @@ export default {
   background-size cover
 .use_place
   background color-bg-default
-  height 800px
+  height 870px
 .product_place
   padding-top 0
 .product
@@ -335,7 +291,7 @@ export default {
   &__product
     border-radius 6px
     width 350px
-    height 310px
+    height 350px
     background color-whitebg
     span
       display inline-block
@@ -360,30 +316,37 @@ export default {
   padding-top 0
   // line-height 45px
   color color-hover
-  height 900px
+  height 670px
   &__title
     font-size 20px
     height 90px
     line-height 90px
   &__box
     overflow hidden
-    padding-left 5px
+    // padding-left 5px
+    height 245px
+    position absolute
+    overflow hidden
+    top 0
+    left 10px
+    background rgba(0,0,0,.6)
   &__product
+    position relative
     border-radius 6px
     width 250px
-    height 350px
+    height 245px
     overflow hidden
     background color-bg-gray
     span
       display inline-block
       font-size 14px
-      color color-hover
+      color #fff
   &__transform
-    box-shadow:2px 2px 5px #999
+    // box-shadow:2px 2px 5px #999
   &__transform:hover
-      transform translateY(-5%)
-      -ms-transform translateY(-5%)
-      -webkit-transform translateY(-5%)
+      // transform translateY(-5%)
+      // -ms-transform translateY(-5%)
+      // -webkit-transform translateY(-5%)
   &__img
     width 230px
     height 192px
@@ -393,9 +356,26 @@ export default {
     height 450px
   &__tip
     width 230px
-    line-height 24px
+    line-height 25px
     text-align left
+    margin 7px
+  &__titles
+    width 100%
+    line-height 45px
+    text-align center
+    font-size 15px
 .row-space
   margin-bottom 40px
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateY(100px);
+  opacity: 0;
+}
 </style>
 
